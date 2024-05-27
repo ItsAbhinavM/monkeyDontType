b64b63b0e27a57d4996984495b8602a3b5bfe9e2
ItsAbhinavM <abhinav051017@gmail.com>
2024-05-27 20:31:32 +0530

    detect first word functionality

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 def get_word_from_web(url):
@@ -10,20 +8,16 @@ def get_word_from_web(url):
     driver.get(url)
     
     try:
-        # Explicit wait for the element to be present
-        word_div = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'word active'))
-        )
-        word = word_div.text
+        word_div = driver.find_element(By.CLASS_NAME, 'word.active')
+        word=word_div.text
+        # input_field=driver.find_element(By)
+        # word_div.send_keys(word)
         print(word)
-        
-        input_field = driver.find_element(By.CLASS_NAME, '')  # Replace with actual class name or ID
-        input_field.send_keys(word)
 
     except Exception as e:
-        print('Failed to find the word active div:', e)
-    finally:
-        driver.quit()
+        print('Failed to find the word-active div:', e)
+    # finally:
+    #     driver.quit()
 
 url = 'https://monkeytype.com/'
 get_word_from_web(url)
