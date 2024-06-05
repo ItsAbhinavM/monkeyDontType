fda2a496ae8ecffac98d69dd7618182398706d88
ItsAbhinavM <abhinav051017@gmail.com>
2024-06-05 21:43:05 +0530

    word detecting and typing functionality

--- main.py
+++ main.py
@@ -2,22 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+import time
 
-def get_word_from_web(url):
+def get_word_from_web(url, delay=0.1):
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.get(url)
     
     try:
-        word_div = driver.find_element(By.CLASS_NAME, 'word.active')
-        word=word_div.text
-        # input_field=driver.find_element(By)
-        # word_div.send_keys(word)
-        print(word)
-
+        while True:
+            word_div = driver.find_element(By.CLASS_NAME, 'word.active')
+            letters = word_div.find_elements(By.TAG_NAME, 'letter')
+            word = ''.join([letter.text for letter in letters])
+            word += ' '
+            ActionChains(driver).send_keys(word).perform()
+            time.sleep(delay)
     except Exception as e:
-        print('Failed to find the word-active div:', e)
-    # finally:
-    #     driver.quit()
+        print('Error:', e)
+    finally:
+        driver.quit()
 
 url = 'https://monkeytype.com/'
 get_word_from_web(url)
