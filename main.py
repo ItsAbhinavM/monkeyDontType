c889603e9e85eaff1e260ecda8884489cbe896de
ItsAbhinavM <abhinav051017@gmail.com>
2024-06-07 22:00:47 +0530

    patch update on functions

--- main.py
+++ main.py
@@ -10,31 +10,42 @@ def get_word_from_web(url, delay=0.1):
     driver.get(url)
     # driver.find_element(By.CSS_SELECTOR,'active acceptAll').click() # sfocus on the page ; the unwanted bug_4ao5eF8B
     try:
-        while True:
-            identifyWords(driver,delay)
-    except Exception as e:
-        endCredits(driver)
+        try:
+            while True:
+                identifyWords(driver,delay)
+        except Exception as e:
+            endCredits(driver)
+    except KeyboardInterrupt:
+        print("keyboard interpution detected")
+        quit()
 
 def identifyWords(driver,delay):
     try:
-        while True:
-            word_div = driver.find_element(By.CLASS_NAME, 'word.active')
-            letters = word_div.find_elements(By.TAG_NAME, 'letter')
-            word = ''.join([letter.text for letter in letters])
-            word += ' '
-            ActionChains(driver).send_keys(word).perform()
-            time.sleep(delay)
-    except Exception as e:
-        endCredits(driver)
+        try:
+            while True:
+                word_div = driver.find_element(By.CLASS_NAME, 'word.active')
+                letters = word_div.find_elements(By.TAG_NAME, 'letter')
+                word = ''.join([letter.text for letter in letters])
+                word += ' '
+                ActionChains(driver).send_keys(word).perform()
+                time.sleep(delay)
+        except Exception as e:
+            endCredits(driver)
+    except KeyboardInterrupt:
+        print("keyboard interpution detected")
+        quit()
 
 def endCredits(driver):
     wpm=driver.find_element(By.CSS_SELECTOR, ".group.wpm").find_element(By.CLASS_NAME,"bottom").text
     acc=driver.find_element(By.CSS_SELECTOR, ".group.acc").find_element(By.CLASS_NAME,"bottom").text
     consistency = driver.find_element(By.CSS_SELECTOR, ".group.flat.consistency").find_element(By.CLASS_NAME, "bottom").text
-    print("wpm: " + wpm)
-    print("accuracy: " + acc)
-    print("consistency: " + consistency)
-    runAgain(driver)
+    if len(wpm)==0:
+        runAgain(driver)
+    else:
+        print("wpm: " + wpm)
+        print("accuracy: " + acc)
+        print("consistency: " + consistency)
+        runAgain(driver)
 
 def runAgain(driver):
     userChoice=input("do you wish to run again (y/n) : ")
@@ -44,8 +55,8 @@ def runAgain(driver):
             next_test_button.click()
             time.sleep(1)
             identifyWords(driver,0.1)
-        except Exception as e:
-            print('Error clicking Next test button:', e)
+        except Exception:
+            print('Error clicking Next test button:')
     elif userChoice=='n':
         print("bye")
         try:
@@ -66,5 +77,6 @@ banner=f"""
 ╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝   ╚═╝      ╚═╝      ╚═╝   ╚═╝     ╚══════╝
                                                                                                                              """
 print(banner)
+print("This version does not support automated cookie detection, kindly click on the accept cookie button for better experience")
 url = 'https://monkeytype.com/'
 get_word_from_web(url)
